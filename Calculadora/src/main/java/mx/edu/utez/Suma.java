f525f9396cd32f6d79fd8866404338f45e0b55f3
SoyCypex <20243ds027@utez.edu.mx>
2025-09-02 12:14:55 -0600

    Aqui esta mi creación, en este caso es la clase llamada suma

---
+++ Calculadora/src/main/java/mx/edu/utez/Suma.java
@@ -0,0 +1,24 @@
+package mx.edu.utez;
+
+public class Suma {
+    private double numero, numero2;
+    public double calculaSuma(double numero, double numero2) {
+        return numero - numero2;
+    }
+
+    public double getNumero() {
+        return numero;
+    }
+
+    public void setNumero(double numero) {
+        this.numero = numero;
+    }
+
+    public double getNumero2() {
+        return numero2;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+}
