ebd8baab1fc01e5c1fd55d9695ee9d4fc37a9b49
ren1989lisa <renata.ortizchavez19@utez.edu.mx>
2025-09-02 12:04:57 -0600

    Se añadio la clase restar

---
+++ Calculadora/src/main/java/mx/edu/utez/Restar.java
@@ -0,0 +1,22 @@
+package mx.edu.utez;
+
+public class Restar {
+    private double num;
+    private double num2;
+    public double calculaResta(double num, double num2) {
+        return num - num2;
+    }
+    public double getNum() {
+        return num;
+    }
+    public void setNum(double num) {
+        this.num = num;
+    }
+    public double getNum2() {
+        return num2;
+    }
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+}
+
