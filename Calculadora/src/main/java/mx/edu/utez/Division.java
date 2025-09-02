57d1a591d3e2f2b3147f48587d09c551e7a41d4c
lum-i3 <dana.bahena.21@gmail.com>
2025-09-02 11:45:27 -0600

    atributos y metodo calculaDivison de Division

--- Calculadora/src/main/java/mx/edu/utez/Division.java
+++ Calculadora/src/main/java/mx/edu/utez/Division.java
@@ -1,5 +1,13 @@
 package mx.edu.utez;
 
 public class Division {
-    
+    private double num;
+    private double num2;
+
+    public double calculaDivision(double num, double num2) {
+        if (num2 == 0) {
+            System.out.println("No se puede dividir entre 0");
+        }
+        return num / num2;
+    }
 }
