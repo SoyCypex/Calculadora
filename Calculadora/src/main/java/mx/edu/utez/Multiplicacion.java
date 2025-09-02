c66604563b1cb4c47251cc5cb14df5822275b645
Ian Alejandro Rivera Torres <20243ds030@utez.edu.mx>
2025-09-02 11:50:50 -0600

    Clase de multiplicacion

---
+++ Calculadora/src/main/java/mx/edu/utez/Multiplicacion.java
@@ -0,0 +1,22 @@
+package mx.edu.utez;
+
+public class Multiplicacion {
+    private double num;
+    private double num2;
+    public double calculaMultiplicacion(double num, double num2) {
+        return num * num2;
+    }
+    public double getNum() {
+        return num;
+    }
+    public void setNum(double num) {
+        this.num = num;
+    }
+    public double getNum2() {
+        return num2;
+    }
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+}
+
